376e96a84a4c243edd4348a237c8c97cc6d5221f
rickgurgel <rgurgelf@gmail.com>
2023-09-13 02:18:27 -0300

    Entity User e REST criados e funcionando

---
+++ src/main/java/com/rickgurgel/chato/entities/User.java
@@ -0,0 +1,21 @@
+package com.rickgurgel.chato.entities;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class User implements Serializable{
+
+    private String id;
+    private String name;
+    private String email;
+}
