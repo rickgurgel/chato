afdfc0e34e26cc690d296dd9472cfbe52b489957
rickgurgel <rgurgelf@gmail.com>
2023-09-13 02:01:13 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/rickgurgel/chato/ChatoApplication.java
@@ -0,0 +1,13 @@
+package com.rickgurgel.chato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatoApplication.class, args);
+	}
+
+}
