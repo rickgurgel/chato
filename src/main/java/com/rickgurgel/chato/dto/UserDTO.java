ec2fbdda63f154a53654ffd96ddb2bbc3603a518
rickgurgel <rgurgelf@gmail.com>
2023-09-13 09:29:03 -0300

    Usando padrao DTO para retornar usuarios

---
+++ src/main/java/com/rickgurgel/chato/dto/UserDTO.java
@@ -0,0 +1,26 @@
+package com.rickgurgel.chato.dto;
+
+import java.io.Serializable;
+
+import com.rickgurgel.chato.domain.User;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserDTO implements Serializable{
+ 
+    private String id;
+    private String name;
+    private String email;
+
+    public UserDTO(User obj){
+        id = obj.getId();
+        name = obj.getName();
+        email = obj.getEmail();
+    } 
+
+}
